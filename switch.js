9871cab1d926a5be2884c11be83cb02c7e51afce
IswaryaNidadavolu <iswaryanidadavolu@gmail.com>
2020-09-04 09:05:20 -0400

    switch statement example

---
+++ switch.js
@@ -0,0 +1,12 @@
+var num=1;
+switch(num){
+    case 1:
+      console.log('is 1');
+      break;
+    case 8:
+       console.log('is 8');
+        break;
+    default:
+        console.log('default');
+        break;
+}
